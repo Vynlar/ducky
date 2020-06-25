174eff49a392444eb6825471fe6936df6d8cf985
Adrian Aleixandre <lightningzygella@gmail.com>
2020-06-25 18:17:41 -0500

    Refactored wizard to be way easier to work with + now we show previous steps

--- src/wizard.tsx
+++ src/wizard.tsx
@@ -5,6 +5,7 @@ import {
   Alert,
   AlertProps,
   AlertIcon,
+  VisuallyHidden,
   Icon,
   Flex,
   Stack,
@@ -28,91 +29,50 @@ const fadeIn = keyframes({
   },
 });
 
-function* cyclicSequence<T>(options: T[]): Generator<T, T, T> {
-  while (true) {
-    for (const option of options) {
-      yield option;
-    }
-  }
+interface StepProps {
+  next?: () => void;
+  isActive?: boolean;
+  isLoading?: boolean;
 }
 
-interface Stage {
-  label: string;
-  helper: string;
-  alert?: {
-    text: string;
-    status: AlertProps["status"];
-  };
+interface StepFooterProps {
+  isValid?: boolean;
 }
 
-function Wizard() {
-  const [loading, setLoading] = useState(false);
-  const [submitCount, setSubmitCount] = useState(0);
+function StepFooter({ isValid }: StepFooterProps) {
+  return (
+    <Stack
+      direction={["column", "row"]}
+      spacing="4"
+      justifyContent="flex-end"
+      alignItems="stretch"
+      w="100%"
+    >
+      <Button as={RLink} to="/success" variant="ghost" colorScheme="yellow">
+        Oh! I figured it out!
+      </Button>
 
-  const promptGenerator = useMemo(
-    () =>
-      cyclicSequence<Stage>([
-        {
-          label: "Briefly describe the problem you're having",
-          helper: "Tip: Don't over think it!",
-        },
-        {
-          label:
-            "Have you tried restarting the server/clearing caches/reinstalling dependencies?",
-          helper: "This gets me pretty often!",
-          alert: {
-            text: "Oops! I remembered something else I need!",
-            status: "warning",
-          },
-        },
-        {
-          label: "List out all the assumptions you've made.",
-          helper: "Tip: Do you best to write down everything you can think of.",
-          alert: {
-            text: "Looks like we need some more info from you",
-            status: "info",
-          },
-        },
-        {
-          label: "Which assumptions do you know are true?",
-          helper:
-            "Tip: Pay extra attention to the ones you think are most obvious!",
-          alert: {
-            text: "Oops! I remembered something else I need!",
-            status: "warning",
-          },
-        },
-        {
-          label: "Where else could the problem be that you haven't looked yet?",
-          helper: "Idea: Perhaps a configuration or environment issue?",
-          alert: {
-            text: "Hmmm. Let's shift gears and give this a try.",
-            status: "info",
-          },
-        },
-      ]),
-    []
+      <Button
+        isDisabled={!isValid}
+        colorScheme="yellow"
+        w={["auto", 40]}
+        type="submit"
+      >
+        Solve
+      </Button>
+    </Stack>
   );
-  const initialPrompt = useMemo(() => {
-    return promptGenerator.next().value;
-  }, [promptGenerator]);
-  const [prompt, setPrompt] = useState(initialPrompt);
+}
 
-  function handleSubmit() {
-    setLoading(true);
-    setSubmitCount((prev) => prev + 1);
-    setPrompt(promptGenerator.next().value);
-
-    let delay =
-      Math.random() < 0.2
-        ? Math.random() * 2000 + 2000
-        : Math.random() * 700 + 500;
-
-    setTimeout(() => {
-      setLoading(false);
-    }, delay);
-  }
-
+interface StepTemplateProps extends StepProps {
+  children: React.ReactNode;
+}
+function StepTemplate({
+  children,
+  next,
+  isActive,
+  isLoading,
+}: StepTemplateProps) {
   return (
     <Box
       as="article"
@@ -122,63 +82,155 @@ function Wizard() {
       px={6}
       py={6}
       w="100%"
+      border="4px solid"
+      borderColor={isActive ? "yellow.400" : "transparent"}
     >
-      {loading ? (
-        <Flex
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          height="48"
-          color="yellow.500"
-        >
-          <Spinner mx="auto" size="lg" mb="2" />
-          <Text animation={`${fadeIn} 0.2s 1s both`}>
-            It's taking longer than usual...
-          </Text>
-        </Flex>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <Stack spacing="4">
-            {submitCount > 0 && prompt.alert && (
-              <Alert status={prompt.alert.status}>
-                <AlertIcon />
-                {prompt.alert.text}
-              </Alert>
-            )}
-
-            <FormControl id="test">
-              <FormLabel>{prompt.label}</FormLabel>
-              <Textarea name="step1" borderColor="gray.500" minH="200px" />
-              <FormHelperText>
-                {prompt.helper}{" "}
-                <Icon color="yellow.600" fontSize="24px" as={AiOutlineSmile} />
-              </FormHelperText>
-            </FormControl>
-
-            <Stack
-              direction={["column", "row"]}
-              spacing="4"
-              justifyContent="flex-end"
-              alignItems="stretch"
-              w="100%"
-            >
-              <Button
-                as={RLink}
-                to="/success"
-                variant="ghost"
-                colorScheme="yellow"
-              >
-                Oh! I figured it out!
-              </Button>
-
-              <Button colorScheme="yellow" w={["auto", 40]} type="submit">
-                Solve
-              </Button>
-            </Stack>
-          </Stack>
-        </form>
-      )}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          next && next();
+        }}
+      >
+        {isLoading && <Loading />}
+        <Stack spacing="4" d={isLoading ? "none" : "flex"}>
+          {children}
+        </Stack>
+      </form>
     </Box>
+  );
+}
+
+function FinalStep(props: StepProps) {
+  return (
+    <StepTemplate {...props}>
+      <Text fontWeight="bold">I'm all fresh out of ideas :(</Text>
+      <Text>Would you like to restart?</Text>
+      <Button as={RLink} to="/" colorScheme="yellow" px={6}>
+        Restart
+      </Button>
+    </StepTemplate>
+  );
+}
+
+interface TextAreaStepProps extends StepProps {
+  label: string;
+  helper: string;
+}
+
+function TextAreaStep(props: TextAreaStepProps) {
+  const [response, setResponse] = useState("");
+
+  return (
+    <StepTemplate {...props}>
+      {props.isActive ? (
+        <>
+          <FormControl>
+            <FormLabel>{props.label}</FormLabel>
+            <Textarea
+              value={response}
+              onChange={(e) => setResponse(e.target.value)}
+              borderColor="gray.500"
+              minH="200px"
+            />
+            <FormHelperText>
+              {props.helper}{" "}
+              <Icon color="yellow.500" fontSize="24px" as={AiOutlineSmile} />
+            </FormHelperText>
+          </FormControl>
+
+          <StepFooter isValid={!!response} />
+        </>
+      ) : (
+        <>
+          <Text fontWeight="bold">{props.label}:</Text>
+          <Text fontStyle="italic">{response}</Text>
+        </>
+      )}
+    </StepTemplate>
+  );
+}
+
+interface StepsProps {
+  children: React.ReactElement[];
+}
+
+function Steps({ children }: StepsProps) {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  function next() {
+    setLoading(true);
+    let delay =
+      Math.random() < 0.2
+        ? Math.random() * 2000 + 2000
+        : Math.random() * 700 + 500;
+
+    setTimeout(() => {
+      setLoading(false);
+      setCurrentStep((old) => old + 1);
+    }, delay);
+  }
+
+  const newChildren = React.Children.map(children, (child, i) => {
+    const props: StepProps = {
+      next,
+      isActive: i === currentStep,
+      isLoading: loading && i === currentStep,
+    };
+    if (i > currentStep) return null;
+    return React.cloneElement(child, props);
+  });
+
+  return (
+    <Stack w="100%" spacing="6">
+      {newChildren}
+    </Stack>
+  );
+}
+
+function Loading() {
+  return (
+    <Flex
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="48"
+      color="yellow.500"
+      w="100%"
+    >
+      <Spinner mx="auto" size="lg" mb="2" />
+      <Text animation={`${fadeIn} 0.2s 1s both`}>
+        It's taking longer than usual...
+      </Text>
+    </Flex>
+  );
+}
+
+function Wizard() {
+  return (
+    <Steps>
+      <TextAreaStep
+        label="Briefly describe the problem you're having"
+        helper="Tip: Don't over think it"
+      />
+      <TextAreaStep
+        label="Have you tried restarting the server/clearing caches/reinstalling dependencies?"
+        helper="This gets me pretty often!"
+      />
+      <TextAreaStep
+        label="List out all the assumptions you've made."
+        helper="Tip: Do you best to write down everything you can think of."
+      />
+      <TextAreaStep
+        label="Which assumptions do you know are true?"
+        helper="Tip: Pay extra attention to the ones you think are most obvious!"
+      />
+      <TextAreaStep
+        label="Where else could the problem be that you haven't looked yet?"
+        helper="Idea: Perhaps a configuration or environment issue?"
+      />
+      <FinalStep />
+    </Steps>
   );
 }
 
